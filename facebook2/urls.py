0295e359f8e22b5cdf85124c5f1de4c6316594af
Md Imam Hossain <71822547+imamtuhin@users.noreply.github.com>
2022-02-10 00:20:35 +0600

    Add files via upload

--- facebook2/urls.py
+++ facebook2/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from . import views
+from django.views.static import serve
+from django.conf.urls import url
 
 
 urlpatterns = [
