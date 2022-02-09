7ca90ec253c466b48bb78bd627403d5b7bbba7f0
Md Imam Hossain <71822547+imamtuhin@users.noreply.github.com>
2022-02-10 00:08:19 +0600

    Add files via upload

--- facebook2/settings.py
+++ facebook2/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-mj*4p*27opn)o5yr4yu^)=i4w*9wy4+cpppss%i36wuhhx*dcw'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
