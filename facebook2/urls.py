803800b51fe90de85ecf3cd6c136d9d4295266e9
Md Imam Hossain <71822547+imamtuhin@users.noreply.github.com>
2022-02-10 00:23:43 +0600

    Add files via upload

--- facebook2/urls.py
+++ facebook2/urls.py
@@ -14,6 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
+from facebook2.views import *
 from django.urls import path
 from . import views
 from django.views.static import serve
